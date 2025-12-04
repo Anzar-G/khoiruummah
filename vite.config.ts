4039e016db22df43d38decaa5839ec99358d1046
Nizar <muhammadniyar282@gmail.com>
2025-12-04 10:19:03 +0700

    Update frontend

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: './', // Use relative paths for assets
+export default defineConfig(({ command }) => {
+    const base = command === 'build' ? '/khoiruummah/' : '/'
+    return {
+        plugins: [react()],
+        base,
+    }
 })
